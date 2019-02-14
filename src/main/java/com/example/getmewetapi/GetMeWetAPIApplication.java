3dc1889630e63f9647b370e8e646c475419d62c5
robin-lunde <robin.lunde@linecorp.com>
2019-02-14 15:21:12 +0900

    init creds

--- src/main/java/com/example/getmewetapi/GetMeWetAPIApplication.java
+++ src/main/java/com/example/getmewetapi/GetMeWetAPIApplication.java
@@ -67,7 +67,6 @@ public class GetMeWetAPIApplication {
             plant = new Plant("test2","---");
             plantService.createPlant(plant);
             //------------------- Status ----------------------
-
             /**/
         };
     }
